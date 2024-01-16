08233a1bc26ab0feac9176f85af421cf5b8957f8
Kelson Camargo <kelson.m.camargo@htomail.com>
2024-01-16 17:17:57 -0300

    feat: User Register added to view/UserView

--- src/view/UserView.js
+++ src/view/UserView.js
@@ -1,7 +1,12 @@
 const EXPRESS = require("express")
 const ROUTER = EXPRESS.Router()
 const MIDDLEWARES = require("../middleware/Middleware")
+const USER = require("../controllers/UserController")
 
 const MIDDLEWARE = new MIDDLEWARES()
+const USER_CONTROLLER = new USER()
 
-ROUTER.post()
+ROUTER.post(
+  '/register',
+  USER_CONTROLLER.USER_REGISTER
+)
